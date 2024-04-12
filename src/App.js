11942797cf35901530668b06d7887733f3f35c40
PedroYVS <pedyshib@gmail.com>
2024-04-12 18:00:58 -0300

    feat(p1-pdmt): exibe lista ficticia

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react'
 import TarefaEntrada from './TarefaEntrada'
+import TarefaLista from './TarefaLista'
 import 'bootstrap/dist/css/bootstrap.css'
 
 export default class App extends Component {
+    state = {
+        tarefas: ['Preparar aula de programação', 'Fazer feira', 'Preparar marmitas']
+    }
+    
     render() {
         return (
         <div className='container text-center'>
-            <TarefaEntrada
-            plholder='Digite a descrição de uma nova tarefa'/>
+            <TarefaEntrada plholder='Digite a descrição de uma nova tarefa'/>
+            <TarefaLista tarefas={this.state.tarefas}/>
         </div>
         )
     }
