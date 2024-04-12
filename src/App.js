80c917e589f3a3aa9d945b7dad7cc0877521c79e
PedroYVS <pedyshib@gmail.com>
2024-04-12 16:54:26 -0300

    feat(p1-pdmt): cria componente principal e faz hello

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import 'bootstrap/dist/css/bootstrap.css'
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className='container text-center'>
+        <div className='row mt-4 justify-content-center'>
+            <div className='col col-xxl-4 col-xl-6 col-lg-8 col-md-12 col-sm-12'>
+                <h1>Hello, tarefas</h1>
+            </div>
+        </div>
+      </div>
+    )
+  }
+}
