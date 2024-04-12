183765d67fa16e4d035a9d686559bf3a6c50d760
PedroYVS <pedyshib@gmail.com>
2024-04-12 17:28:27 -0300

    feat(p1-pdmt): cria componente de entrada

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import React, { Component } from 'react'
+import TarefaEntrada from './TarefaEntrada'
 import 'bootstrap/dist/css/bootstrap.css'
 
 export default class App extends Component {
-  render() {
-    return (
-      <div className='container text-center'>
-        <div className='row mt-4 justify-content-center'>
-            <div className='col col-xxl-4 col-xl-6 col-lg-8 col-md-12 col-sm-12'>
-                <h1>Hello, tarefas</h1>
-            </div>
+    render() {
+        return (
+        <div className='container text-center'>
+            <TarefaEntrada
+            plholder='Digite a descrição de uma nova tarefa'/>
         </div>
-      </div>
-    )
-  }
+        )
+    }
 }
